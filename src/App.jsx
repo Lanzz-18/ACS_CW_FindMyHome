ae287bd6fd82df28062c43fa101a438c69160584
Senal Perera <senalperera777@gmail.com>
2026-01-02 18:42:52 +0530

    Search bar fix  + nav bar fix+ new files added
    - made the search bar in the main home page with styling
    - added nav bar links
    - added image files
    - added a footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,35 @@ import React from 'react';
 import SearchBar from './components/SearchBar';
 import PropertyCard from './components/PropertyCard';
 import PropertyPage from './components/PropertyPage';
+import NavBar from "./components/NavBar";
+import homeImage from "./assets/homes.jpg"
 
 function App(){
   return(
     <div>
-      <h1>Test</h1>
-      <SearchBar />
+      <NavBar />
+
+      {/* Image card */}
+      <div className="main-panel">
+        <h1><span>Believe</span> in finding it</h1>
+        <h3>With the UK's largest choice of homes</h3>
+
+        <div className="search-panel">
+          <ul className="search-option-container">
+            <li className="search-option">Buy</li>
+            <li className="search-option">Rent</li>
+            <li className="search-option">Sold</li>
+          </ul>
+          <hr />
+          <form className="search-panel-form">
+            <div className="search-group">
+              <label for="search">Search properties to buy</label>
+              <input type="search" id="search" placeholder='Bath, NW3, or Leeds station'></input>
+            </div>
+            <button id="search-button">Search</button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 const NavBar = () => {
     return(
-        <div>
+        <div className="nav">
+            <div id="logo">
+                <a href="#">SiteLogo</a>
+            </div>
+            <div className="nav-links">
+                <a href="#">Buy</a>
+                <a href="#">Rent</a>
+                <a href="#">Find Agent</a>
+                <a href="#">House Prices</a>
+            </div>
             
         </div>
     )
