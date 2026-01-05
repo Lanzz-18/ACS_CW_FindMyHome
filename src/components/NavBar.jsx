66e2845397d985533f97a2174f5435f3e3c3c26a
Senal Perera <senalperera777@gmail.com>
2026-01-05 11:22:24 +0530

    Create NavBar.jsx

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '/images/logo.png';
+import { Link } from 'react-router-dom'; 
+
+const NavBar = () => {
+    return(
+        <div className="nav">
+            <Link to="/" id="logo">
+                <img src={Logo}></img>
+                <p>FindMy<span>Home</span></p>
+            </Link>
+            <div className="nav-links">
+                <Link to="/search-page"><p>Buy Properties</p></Link>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
